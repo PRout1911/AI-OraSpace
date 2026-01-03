9dd82e1fbab1ae4e9a2c88f47a99047015ba2670
PRout1911 <routpriyanka1911@gmail.com>
2026-01-03 19:09:48 +0530

    return statement added

--- alerts/alert.py
+++ alerts/alert.py
@@ -3,5 +3,6 @@ def tablepsace_alert(df):
     df["used %"] = df["used %"].astype(float)
 
     danger = df[df["used %"] >= 95]
-
     warning = df[(df["used %"] >= 85 & df["used %"] < 95)]
+
+    return warning, danger
