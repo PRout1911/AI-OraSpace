0a9948d7dce8fd087dcf31024ea6f67190327405
PRout1911 <routpriyanka1911@gmail.com>
2026-01-03 19:08:01 +0530

    danger condition & warning condition added

--- alerts/alert.py
+++ alerts/alert.py
@@ -2,4 +2,6 @@ def tablepsace_alert(df):
 
     df["used %"] = df["used %"].astype(float)
 
-    threshold = df[df["used %"] >= 95]
+    danger = df[df["used %"] >= 95]
+
+    warning = df[(df["used %"] >= 85 & df["used %"] < 95)]
