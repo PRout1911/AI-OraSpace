26f293742ed055a04f92b4ee0e86ffae4ed6e48c
PRout1911 <routpriyanka1911@gmail.com>
2026-01-03 19:40:50 +0530

    streamlit set page

--- alerts/streamlit_app.py
+++ alerts/streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd
 from db.oracle_conn import get_connection
 from db.sql_queries import TABLESPACE_USAGE_SQL, DATAFILE_SQL
 from db.command_parsar import parsar_command
+from db.utils_alerts import tablespace_alerts
+
+st.set_page
