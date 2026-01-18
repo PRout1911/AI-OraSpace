94e584fa2ee83d4b52783aff77506f6029498dab
PRout1911 <routpriyanka1911@gmail.com>
2026-01-18 22:09:57 +0530

    parse change

--- alerts/streamlit_app.py
+++ alerts/streamlit_app.py
@@ -7,7 +7,7 @@ from db.command_parsar import parsar_command
 from db.utils_alerts import tablespace_alerts
 
 st.set_page_config(page_title="AI-Oraspace", layout="wide")
-st.title("AI-OraSpace – Oracle DBA Command Console")
+st.title("AI-OraSpace - Oracle DBA Command Console")
 
 command = st.text_input(
     "Enter Command", 
