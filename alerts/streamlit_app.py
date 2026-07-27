46915cdf746230ca3ee65089507f48f38ad22eaa
PRout1911 <routpriyanka1911@gmail.com>
2026-07-27 20:39:20 +0530

    modified stramlit_app.py

--- alerts/streamlit_app.py
+++ alerts/streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 from db.oracle_conn import get_connection
 from db.sql_queries import TABLESPACE_USAGE_SQL, DATAFILE_SQL
-from db.command_parsar import parsar_command
-from db.utils_alerts import tablespace_alerts
+from db.command_parser import parse_command
+from utils.alerts import tablespace_alerts
 
 st.set_page_config(page_title="AI-Oraspace", layout="wide")
 st.title("AI-OraSpace - Oracle DBA Command Console")
